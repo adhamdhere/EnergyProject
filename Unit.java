ea1376b7c6ff381a49c8a36df8b4f1530dbc093e
adhamdhere <60016177+adhamdhere@users.noreply.github.com>
2020-02-22 20:44:11 -0500

    Add files via upload

---
+++ Unit.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+
+
+public class Unit {
+    // instance variables
+
+    double latitude;
+    double longitude;
+    int[] coordinates;
+
+    public static void main(String args[]){
+        // want it to return proper output values;
+    }
+
+    // Constructor, with all required inputs for model
+    public Unit(double lt, double lg, int[] cr){
+        latitude = lt;
+        longitude = lg;
+        coordinates = cr;
+    }
+
+    private double area(int[] c){
+        double area = 0.0;
+        // method should return the area of the inputted polygon
+        return area;
+    }
+}
