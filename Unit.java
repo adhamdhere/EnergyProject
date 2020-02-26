19816460553aa7caad30a6c732ac75f58b8728d1
adhamdhere <60016177+adhamdhere@users.noreply.github.com>
2020-02-25 20:34:50 -0500

    Add files via upload
    new update to unit class. pairs with DataFetch, able to get static weather data via call to DarkSky API

--- Unit.java
+++ Unit.java
@@ -1,19 +1,19 @@
 import java.awt.*;
 
-
 public class Unit {
     // instance variables
 
     double latitude;
     double longitude;
-    int[] coordinates;
+    double[] coordinates;
 
     public static void main(String args[]){
         // want it to return proper output values;
+        DataFetch.getAverageRain();
     }
 
     // Constructor, with all required inputs for model
-    public Unit(double lt, double lg, int[] cr){
+    public Unit(double lt, double lg, double[] cr){
         latitude = lt;
         longitude = lg;
         coordinates = cr;
@@ -23,5 +23,11 @@ public class Unit {
         double area = 0.0;
         // method should return the area of the inputted polygon
         return area;
+    }
+
+    private double averagerainfall(double latitude, double longitude){
+        // method needs to use getrainavg in DataFetch class, pass lat and long to get data returned
+        DataFetch.getAverageRain();
+        return 0.0;
     }
 }
