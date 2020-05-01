b21826579a9cb299d9fbcc14dff6cec857c13e24
adhamdhere <adhamdhe@iu.edu>
2020-05-01 15:11:59 -0400

    some more small changes

--- RooftopResources/WebContent/maps/maps.js
+++ RooftopResources/WebContent/maps/maps.js
@@ -42,8 +42,8 @@ function initAutocomplete() {
     var i = 0;
     var drawingManager;
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.674, lng: -73.945},
-        zoom: 12,
+        center: {lat: 39.1724, lng: -86.5216},
+        zoom: 16.5,
         mapTypeId: 'satellite',
         styles: [
             {
