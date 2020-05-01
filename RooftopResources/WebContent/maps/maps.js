dcb8c6e37de508645e880be8543429f15b495f7b
sikapoor <sikapoor@iu.edu>
2020-05-01 14:14:53 +0530

    complete v 1.0

--- RooftopResources/WebContent/maps/maps.js
+++ RooftopResources/WebContent/maps/maps.js
@@ -447,21 +447,10 @@ function initAutocomplete() {
             $.post(url, jsonString, function(data, status){
             	console.log(data)
             });
+
+            document.getElementById("water").innerHTML = data['waterSavings'];
+            document.getElementById("electricity").innerHTML = data['electricity'];
             
-            //old server call, ignore
-          /*  var http = new XMLHttpRequest();
-            var url = 'http://localhost:8080/RooftopResources/RooftopNaturalResources';
-
-            //Send the proper header information along with the request
-            http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-            http.onreadystatechange = function() {//Call a function when the state changes.
-                if(http.readyState == 4 && http.status == 200) {
-                    var response = http.responseText;
-                    console.log(response);
-                }
-            }
-            http.send(result);*/
             
         }
         });
