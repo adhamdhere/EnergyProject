075cd59ab368a15c8d851b6b8367f790fef380d3
adhamdhere <60016177+adhamdhere@users.noreply.github.com>
2020-07-06 13:42:14 -0400

    Update Constants.java

--- RooftopResources/src/com/rtnr/utils/Constants.java
+++ RooftopResources/src/com/rtnr/utils/Constants.java
@@ -4,7 +4,6 @@ public class Constants {
 	
     public static String DARK_SKY_BASE_URL="";
     public static String OPEN_WEATHER_BASE_URL="";
-    public static String OPEN_WEATHER_FILE_URL = "file:/Users/advaitdhamdhere/data/weather.json";
     public static String METEO_STAT_STATION_ID_URL = "https://api.meteostat.net/v1/stations/nearby?";
     public static String METEO_STAT_CLIMATE_DATA_URL = "https://api.meteostat.net/v1/climate/normals?station=";
     public static double FEET_SQUARED_TO_METERES_SQUARED = 10.764;
